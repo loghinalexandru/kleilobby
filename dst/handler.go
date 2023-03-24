1143cdbca6757f30fcaf817a25b3d68923d81477
Loghin Alexandru <loghinalexandru61@gmail.com>
2023-03-24 20:15:27 +0200

    Add some experimental code

--- dst/handler.go
+++ dst/handler.go
@@ -1,8 +1,14 @@
 package dst
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/loghinalexandru/klei-lobby/models"
 )
 
 type Handler struct {
@@ -17,8 +23,37 @@ func NewHandler(l *log.Logger) *Handler {
 
 func (h *Handler) SetupRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/all", h.All)
+	mux.HandleFunc("/rowid", h.RowId)
 }
 
 func (h *Handler) All(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(http.StatusOK)
+}
+
+func (h *Handler) RowId(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(http.StatusOK)
+
+	payload := fmt.Sprintf("{\"__gameId\": \"DontStarveTogether\",\"__token\": \"%v\", \"query\":{\"__rowId\":\"%v\"}}}", request.URL.Query().Get("token"), request.URL.Path)
+
+	kleiRequest, err := http.NewRequest(
+		"POST",
+		"https://lobby-v2-eu-central-1.klei.com/lobby/read",
+		strings.NewReader(payload))
+
+	if err != nil {
+		h.logger.Println(err)
+	}
+
+	kleiRequest.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	result, err := http.DefaultClient.Do(kleiRequest)
+
+	if err != nil {
+		h.logger.Println(err)
+	}
+
+	content, _ := io.ReadAll(result.Body)
+	model := &models.RequestWrapper{}
+
+	json.Unmarshal(content, model)
 }
