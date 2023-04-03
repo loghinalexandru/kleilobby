8d93188e00e2dccdc270f0174cff96fd0389698b
Loghin Alexandru <loghinalexandru61@gmail.com>
2023-04-03 22:21:44 +0300

    Add unit tests for caching

--- caching/caching.go
+++ caching/caching.go
@@ -6,18 +6,18 @@ import (
 )
 
 type Cache[T any] struct {
-	data    map[string]T
-	time    map[string]time.Time
-	expTime time.Duration
-	mtx     sync.RWMutex
+	data map[string]T
+	time map[string]time.Time
+	TTL  time.Duration
+	mtx  sync.RWMutex
 }
 
 func New[T any](expTime time.Duration) *Cache[T] {
 	return &Cache[T]{
-		data:    make(map[string]T),
-		time:    make(map[string]time.Time),
-		expTime: expTime,
-		mtx:     sync.RWMutex{},
+		data: make(map[string]T),
+		time: make(map[string]time.Time),
+		TTL:  expTime,
+		mtx:  sync.RWMutex{},
 	}
 }
 
@@ -35,28 +35,14 @@ func (c *Cache[T]) Get(key string) T {
 	return result
 }
 
-func (c *Cache[T]) GetTimestamp(key string) time.Time {
-	c.mtx.RLock()
-	defer c.mtx.RUnlock()
-
-	return c.time[key]
-}
-
 func (c *Cache[T]) Add(key string, object T) T {
 	c.mtx.Lock()
 	defer c.mtx.Unlock()
 
 	c.data[key] = object
-	c.time[key] = time.Now().UTC().Add(c.expTime)
+	c.time[key] = time.Now().UTC().Add(c.TTL)
 
 	return object
-}
-
-func (c *Cache[T]) Clear() {
-	c.mtx.Lock()
-	defer c.mtx.Unlock()
-
-	c.data = make(map[string]T)
 }
 
 func (c *Cache[T]) Contains(key string) bool {
